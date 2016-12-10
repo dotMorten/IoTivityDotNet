474a5a6f603de4d4e7a05a074b175f38cc841ee3
Morten Nielsen <mn@iter.dk>
2016-12-09 20:04:55 -0800

    Initial Android work

---
+++ src/ClientTestApp.Android/MainActivity.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using IotivityDotNet.Interop;
+
+namespace ClientTestApp.Android
+{
+    [Activity(Label = "ClientTestApp.Android", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+            TextView status = FindViewById<TextView>(Resource.Id.StatusTextView);
+
+            try
+            {
+
+                //Uncomment this to test if the lib is loading correctly:
+                Java.Lang.Runtime.GetRuntime().LoadLibrary("liboctbstack");
+
+                var result = OCStack.OCInit(null, 0, OCMode.OC_CLIENT);
+            }
+            catch(System.Exception ex)
+            {
+                status.Text = ex.Message + "\n" + ex.StackTrace;
+            }
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
--- src/IotivityDotNet.Interop/OCImports.cs
+++ src/IotivityDotNet.Interop/OCImports.cs
@@ -2,6 +2,10 @@
 {
     internal static class Constants
     {
+#if __ANDROID__
+        internal const string DLL_IMPORT_TARGET = "liboctbstack.so";
+#else
         internal const string DLL_IMPORT_TARGET = "octbstack.dll";
+#endif
     }
 }

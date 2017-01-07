78d0a5bcfd46420738c8a1181f2bf8f7e04ffcf7
Morten Nielsen <mn@iter.dk>
2017-01-07 12:23:02 -0800

    Added missing file

---
+++ src/IotivityDotNet/SecurityPayload.cs
@@ -0,0 +1,31 @@
+﻿using IotivityDotNet.Interop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IotivityDotNet
+{
+    public class SecurityPayload : Payload
+    {
+        internal SecurityPayload(IntPtr ptr) 
+        {
+            var _resource = Marshal.PtrToStructure<OCSecurityPayload>(ptr);
+            SecurityData = _resource.SecurityData;
+        }
+
+        public SecurityPayload(byte[] securityData)
+        {
+            SecurityData = securityData;
+        }
+
+        public byte[] SecurityData { get; }
+
+        internal IntPtr AsOCSecurityPayload()
+        {
+            return Interop.OCPayloadInterop.OCSecurityPayloadCreate(SecurityData, (UIntPtr)SecurityData.Length);
+        }
+    }
+}
